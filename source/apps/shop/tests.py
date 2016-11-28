dbcaa12a699af23c0b65e945be4de713e18a76d5
diego <puertorojas@gmail.com>
2016-11-27 23:18:39 -0500

    Se implementa test unitario para el atributo __str__ del modelo Producto

--- source/apps/shop/tests.py
+++ source/apps/shop/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from .models import Category
+from .models import Category, Product
+from datetime import datetime
 
 # model test
 class CategoryTest(TestCase):
@@ -11,3 +12,19 @@ class CategoryTest(TestCase):
                 c = self.create_category()
                 self.assertTrue(isinstance(c, Category))
                 self.assertEqual(c.__str__(), c.name)
+
+class ProductTest(TestCase):
+
+
+        def create_category(self, name="exportaciones", slug="exportaciones-colombia"):
+                return Category.objects.create(name=name, slug=slug)
+
+        def create_product(self, category="", name="exportaciones_bolivia", slug="exportaciones-bolivia", description="exportaciones a Bolivia", available="True", created=datetime.now(), updated=datetime.now()):
+                return Product.objects.create(category=category, name=name, slug=slug, description=description, available=available, created=created, updated=updated)
+
+
+        def test_create_product(self):
+                c = self.create_category()
+                p = self.create_product(category=c)
+                self.assertTrue(isinstance(p, Product))
+                self.assertEqual(p.__str__(), p.name)
