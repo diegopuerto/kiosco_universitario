b14c97870cc2e74e3da938e6e297e1d8ef83e533
diego <puertorojas@gmail.com>
2016-11-28 16:03:42 -0500

    Se implementa test unitario para la funcion product_list de las vistas de shop

--- source/apps/shop/tests.py
+++ source/apps/shop/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from .models import Category, Product
 from datetime import datetime
-
+from django.core.urlresolvers import reverse
 # model test
 class CategoryTest(TestCase):
 
@@ -55,3 +55,23 @@ class ProductTestReverse(TestCase):
                 self.assertTrue(isinstance(c, Category))
                 self.assertTrue(isinstance(p, Product))
                 self.assertEqual(p.get_absolute_url(), '/shop/1/exportaciones-bolivia/')
+
+
+#########view's tests####################################################
+
+class ProductListTest(TestCase):
+
+        
+        def create_category(self, name="exportaciones", slug="exportaciones-colombia"):
+                return Category.objects.create(name=name, slug=slug)
+
+        def create_product(self, category="", name="exportaciones_bolivia", slug="exportaciones-bolivia", description="exportaciones a Bolivia", available="True", created=datetime.now(), updated=datetime.now()):
+                return Product.objects.create(category=category, name=name, slug=slug, description=description, available=available, created=created, updated=updated)
+
+        def test_product_list(self):
+                c = self.create_category()
+                p = self.create_product(category=c)
+                self.assertTrue(isinstance(c, Category))
+                self.assertTrue(isinstance(p, Product))
+                response = self.client.get(reverse('shop:product_list'))
+                self.assertEqual(response.status_code, 200)
